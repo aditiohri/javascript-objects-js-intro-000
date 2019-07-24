1649b9ec34192aa0fc40a11ddc879b2a22749a92
Aditi Ohri <didiohri@gmail.com>
2019-07-24 23:57:49 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {
-chris isaac: "wicked game",
-tierra whack: "only child",
-velvet underground: "pale blue eyes"
+"chris isaac": 'wicked game',
+"tierra whack": 'only child',
+"velvet underground": 'pale blue eyes'
 };
 
 function updatePlaylist(object, key, value) {
